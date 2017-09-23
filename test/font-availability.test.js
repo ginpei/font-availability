5455a5e87d562c99f7eb5d9460d20ead313c3466
ginpei <ginpei@gmail.com>
2017-09-23 10:17:59 -0700

    group

--- test/font-availability.test.js
+++ test/font-availability.test.js
@@ -15,28 +15,30 @@ describe('font-availability', () => {
 		mock.restore()
 	})
 
-	it('start a new session for a new font', () => {
-		const spy = sinon.spy()
-		mock.expects('createNew')
-			.returns({ waitFor: spy })
-			.once()
+	describe('waitFor()', () => {
+		it('start a new session for a new font', () => {
+			const spy = sinon.spy()
+			mock.expects('createNew')
+				.returns({ waitFor: spy })
+				.once()
 
-		fontAvailability.waitFor('MyFont')
+			fontAvailability.waitFor('MyFont')
 
-		expect(spy.withArgs('MyFont').calledOnce).to.equal(true)
-		mock.verify()
-	})
+			expect(spy.withArgs('MyFont').calledOnce).to.equal(true)
+			mock.verify()
+		})
 
-	it('reuse an existing session for a font called before', () => {
-		const spy = sinon.spy()
-		mock.expects('createNew')
-			.returns({ waitFor: spy })
-			.once()
+		it('reuse an existing session for a font called before', () => {
+			const spy = sinon.spy()
+			mock.expects('createNew')
+				.returns({ waitFor: spy })
+				.once()
 
-		const result0 = fontAvailability.waitFor('MyFont')
-		const result1 = fontAvailability.waitFor('MyFont')
+			const result0 = fontAvailability.waitFor('MyFont')
+			const result1 = fontAvailability.waitFor('MyFont')
 
-		expect(result0).to.equal(result1)
-		mock.verify()
+			expect(result0).to.equal(result1)
+			mock.verify()
+		})
 	})
 })
